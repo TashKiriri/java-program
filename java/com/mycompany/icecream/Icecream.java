80df0e18135c5a2297d7df0dcc537ad7095c8a4b
TashKiriri <124256851+TashKiriri@users.noreply.github.com>
2023-07-09 13:59:18 +0300

    Add files via upload

---
+++ java/com/mycompany/icecream/Icecream.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icecream;
+
+/**
+ *
+ * @author HP
+ */
+public class Icecream {
+
+    public static void main(String[] args) {
+       
+    }
+}
